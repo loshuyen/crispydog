df9b2e65c8b0f8f300e4b5fb64dadf4dd409b772
Shuyen Lo <sycw723@gmail.com>
2024-07-27 23:51:45 +0800

    Add library page

---
+++ static/js/library.js
@@ -0,0 +1,8 @@
+//TODO: for展示用
+const item = document.querySelector(".library__item");
+
+document.addEventListener("DOMContentLoaded", () => {
+    item.addEventListener("click", () => {
+        window.location.href = "/property";
+    });
+})

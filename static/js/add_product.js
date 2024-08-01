65444f26ce195b093b341f8e779b126fb813946a
Shuyen Lo <sycw723@gmail.com>
2024-08-01 14:14:22 +0800

    Add upload cover image button controller

---
+++ static/js/add_product.js
@@ -0,0 +1,10 @@
+const upload_cover_btn = document.querySelector(".product__cover-btn");
+const upload_input = document.querySelector(".product__cover-container > input");
+const add_item_btn = document.querySelector(".product__add-btn");
+
+document.addEventListener("DOMContentLoaded", () => {
+    upload_cover_btn.addEventListener("click", () => {
+        upload_input.click();
+    });
+    add_item_btn.addEventListener("click", () => {});
+});

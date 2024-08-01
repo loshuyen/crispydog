e7901bb366d30482f45bd035a34ad9c7b57b5ea6
Shuyen Lo <sycw723@gmail.com>
2024-08-01 17:42:39 +0800

    Add index.html UI

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+const product_cards = document.querySelectorAll(".gallery__product");
+
+document.addEventListener("DOMContentLoaded", () => {
+    product_cards.forEach(card => {
+        card.addEventListener("click", () => {
+            window.location.href = "/product/1";
+        })
+    })
+});

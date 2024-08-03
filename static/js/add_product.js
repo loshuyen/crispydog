a28e6e47cddeb6ec0390e2bfecdaa7024b754012
Shuyen Lo <sycw723@gmail.com>
2024-08-03 17:14:11 +0800

    Add frontend thumbnail file uploading interaction

--- static/js/add_product.js
+++ static/js/add_product.js
@@ -3,10 +3,17 @@ import * as dashboard from "./dashboard.js";
 const upload_cover_btn = document.querySelector(".product__cover-btn");
 const upload_input = document.querySelector(".product__cover-container > input");
 const add_item_btn = document.querySelector(".product__add-item-btn");
+const thumbnail_input = document.querySelector("#thumbnail_input");
+const chosen_file = document.querySelector(".product__cover-chosen");
 
 document.addEventListener("DOMContentLoaded", () => {
     upload_cover_btn.addEventListener("click", () => {
         upload_input.click();
     });
     add_item_btn.addEventListener("click", () => {});
+    thumbnail_input.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        chosen_file.textContent = file.name;
+        chosen_file.style.display = "flex";
+    });
 });

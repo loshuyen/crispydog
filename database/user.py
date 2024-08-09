6d72949773b940fca9db4940b7cb9c6b3b75eae1
Shuyen Lo <sycw723@gmail.com>
2024-08-09 10:29:40 +0800

    Modify user API

---
+++ database/user.py
@@ -0,0 +1,61 @@
+from .database import pool
+
+def verify_password(username, password):
+    try:
+        db = pool.get_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT id, username FROM user WHERE username = %s AND password = %s", (username, password))
+        user = cursor.fetchall()[0]
+        if user:
+            return {"id": user[0], "username": user[1]}
+        else:
+            return None
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        db.close()
+
+def get_username_by_id(id):
+    try:
+        db = pool.get_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT username FROM user WHERE id = %s", (id, ))
+        user = cursor.fetchall()[0]
+        if user:
+            return user[0]
+        else:
+            return None
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        db.close()
+
+def get_user_by_username(username):
+    try:
+        db = pool.get_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT id FROM user WHERE username = %s", (username, ))
+        user = cursor.fetchall()[0]
+        if user:
+            return user[0]
+        else:
+            return None
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        db.close()
+
+def add_user(username, password):
+    try:
+        db = pool.get_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO user (username, password) VALUES (%s, %s)", (username, password))
+        db.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        db.close()
--- routers/user.py
+++ routers/user.py
@@ -4,8 +4,8 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 import jwt
 import os
 import datetime
-from models.user import UserIn, UserResponse
-from database.crud import verify_password, get_username_by_id, get_user_by_username, add_user
+from models import user as model
+from database import user as db
 
 router = APIRouter()
 security = HTTPBearer(auto_error=False)
@@ -14,7 +14,7 @@ def get_auth_user(credentials: HTTPAuthorizationCredentials = Depends(security))
     try:
         token = credentials.credentials
         decode_data = jwt.decode(token, os.getenv("TOKEN_SECRET_KEY"), algorithms="HS256")
-        username_from_db = get_username_by_id(decode_data["id"])
+        username_from_db = db.get_username_by_id(decode_data["id"])
         if username_from_db == decode_data["username"]:
             return decode_data
         else:
@@ -23,13 +23,13 @@ def get_auth_user(credentials: HTTPAuthorizationCredentials = Depends(security))
         return None
 	
 @router.get("/api/user/auth")
-def get_current_user(user = Depends(get_auth_user)) -> UserResponse:
+def get_current_user(user = Depends(get_auth_user)) -> model.UserResponse:
     return {"data": {"id": user["id"], "username": user["username"]}}
 
 @router.put("/api/user/auth")
-def login(user: UserIn):
+def login(user: model.UserIn):
     try:
-        user_data = verify_password(user.username, user.password)
+        user_data = db.verify_password(user.username, user.password)
         if not user_data:
             return JSONResponse(status_code=400, content={"error": True, "message": "登入失敗，Email、密碼錯誤或尚未註冊"})
         current_utc_time = datetime.datetime.now()
@@ -38,16 +38,17 @@ def login(user: UserIn):
         user_data["exp"] = future_unix_timestamp
         token = jwt.encode(user_data, os.getenv("TOKEN_SECRET_KEY"), algorithm="HS256")
         return JSONResponse(status_code=200, content={"token": token})
-    except:
+    except Exception as e:
+        print(e)
         return JSONResponse(status_code=500, content={"error": True, "message": "伺服器內部錯誤"})
 
 @router.post("/api/user")
-def signup(user: UserIn):
+def signup(user: model.UserIn):
     try:
-        exist_user = get_user_by_username(user.username)
+        exist_user = db.get_user_by_username(user.username)
         if exist_user:
             return JSONResponse(status_code=400, content={"error": True, "message": "註冊失敗，重複的Email"})
-        add_user(user.username, user.password)
+        db.add_user(user.username, user.password)
         return JSONResponse(status_code=200, content={"ok": True})
     except:
         return JSONResponse(status_code=500, content={"error": True, "message": "伺服器內部錯誤"})

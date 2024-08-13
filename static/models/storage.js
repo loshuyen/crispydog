4bb8d5ed246927618791750d3902f6f6f3e82fe9
Shuyen Lo <sycw723@gmail.com>
2024-08-14 00:25:50 +0800

    Add frontend storage model

--- static/models/storage.js
+++ static/models/storage.js
@@ -1,7 +1,28 @@
-import { fetch_with_token } from "./user.js";
+import {fetch_with_token} from "./user.js";
 
 export async function get_all_storage() {
     const response = await fetch_with_token("/api/storage", "GET");
     const data = await response.json();
     return data.data;
 }
+
+export async function get_storage_by_product_id(id) {
+    const response = await fetch_with_token(`/api/storage?product_id=${id}`, "GET");
+    const data = await response.json();
+    return data.data;
+}
+
+export async function download_file(endpoint, file_name) {
+    const response = await fetch_with_token(`/api/storage/download/${endpoint}`, "GET");
+    if (response.status === 200) {
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.target = "_blank";
+        a.download = file_name;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    }
+ }

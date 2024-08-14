1ff9ef1aa364a74fafc56e708c2f749c838d9a8d
Shuyen Lo <sycw723@gmail.com>
2024-08-14 11:18:04 +0800

    Modify sale API response data

--- database/sale.py
+++ database/sale.py
@@ -6,18 +6,21 @@ def get_all_sales(user_id):
         db = pool.get_connection()
         cursor = db.cursor()
         cursor.execute("""
-            SELECT product.id, product.name, SUM(product.price), COUNT(product.id) 
-            FROM sale INNER JOIN product ON sale.product_id = product.id
+            SELECT product.id, product.name, product.price, SUM(product.price), COUNT(product.id), product.thumbnail_url, product.status 
+            FROM product LEFT JOIN sale ON sale.product_id = product.id
             WHERE product.owner_id = %s
             GROUP BY product.id;""", (user_id, ))
         sales = cursor.fetchall()
         result = []
         for sale in sales:
-            product_id, product_name, revenue, sale_count = sale
+            product_id, product_name, product_price, revenue, sale_count, product_thumbnail, product_status = sale
             result.append({
                 "product": {
                     "id": product_id,
                     "name": product_name,
+                    "price": product_price,
+                    "thumbnail": product_thumbnail,
+                    "status": product_status,
                     "revenue": int(revenue),
                     "sales": sale_count
                 }

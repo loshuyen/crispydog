998d754834c0aa4b720abaafc52582630f33631b
Shuyen Lo <sycw723@gmail.com>
2024-08-16 20:47:16 +0800

    Modify google oauth config

--- utils/google_auth.py
+++ utils/google_auth.py
@@ -1,9 +1,19 @@
 import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
 import os
-
-flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    "/Users/shuyen/Desktop/projects/crispydog/static/google_secret.json",
+print(os.getcwd())
+flow = google_auth_oauthlib.flow.Flow.from_client_config(
+    {
+        "installed":
+        {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+            "project_id": "crispydog",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret": os.getenv("GOOGLE_CLIENT_KEY"),
+            "redirect_uris": ["http://localhost", "https://crispydog.xyz"]
+        }
+    },
     scopes=["https://www.googleapis.com/auth/userinfo.profile"])
 flow.redirect_uri = os.getenv("GOOGLE_AUTH_REDIRECT_URL")
 

7b103e916c66c4358e05ee095b01642753565a4c
Shuyen Lo <sycw723@gmail.com>
2024-08-16 16:24:10 +0800

    Render username in the member page

--- static/controllers/dashboard.js
+++ static/controllers/dashboard.js
@@ -3,6 +3,7 @@ import {fetch_auth_user} from "../models/user.js";
 const website_title = document.querySelector(".dashboard__website-title");
 const library = document.querySelector(".dashboard__item-library");
 const store = document.querySelector(".dashboard__item-store");
+const sidebar = document.querySelector(".dashboard__left");
 
 document.addEventListener("DOMContentLoaded", async () => {
     const user = await fetch_auth_user();
@@ -21,4 +22,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     store.addEventListener("click", () => {
         window.location.href = "/store";
     });
+
+    const member = document.createElement("div");
+    member.className = "dashboard__item-member";
+    member.innerHTML = `<img src="/static/icons/user.svg">${user.username}`;
+    sidebar.appendChild(member);
 });

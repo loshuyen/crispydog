d3210a2cd3337c318d3931de043d333146abbd15
Shuyen Lo <sycw723@gmail.com>
2024-08-26 01:03:06 +0800

    Add dashboard notification count rendering.

--- static/controllers/dashboard.js
+++ static/controllers/dashboard.js
@@ -1,4 +1,5 @@
 import {fetch_auth_user} from "../models/user.js";
+import {get_notifications} from "../models/notification.js";
 
 const website_title = document.querySelector(".dashboard__website-title");
 const library = document.querySelector(".dashboard__item-library");
@@ -8,12 +9,45 @@ const notification = document.querySelector(".dashboard__item-notification");
 const commission = document.querySelector(".dashboard__item-commission");
 const work = document.querySelector(".dashboard__item-work");
 
+function update_notification_count(count) {
+    const conut_display = document.querySelector(".dashboard__notification-count");
+    if (count > 0) {
+        conut_display.textContent = count;
+        conut_display.style.display = "flex";
+    } else {
+        conut_display.style.display = "none";
+    }
+}
+
+async function refresh_notification() {
+    notifications = await get_notifications();
+    update_notification_count(notitfications_count);
+}
+
+const token = localStorage.getItem("token");
+const ws = new WebSocket(`ws://localhost:8000/api/notification?token=${token}`);
+ws.onmessage = async function(event) {
+    console.log(event.data);
+    notitfications_count++;
+    await refresh_notification();
+};
+
+let notifications;
+let notitfications_count = 0;
 document.addEventListener("DOMContentLoaded", async () => {
     const user = await fetch_auth_user();
     if (!user) {
         window.location.href = "/";
     }
     
+    notifications = await get_notifications();
+    notifications?.forEach(element => {
+        if (element.notification.is_read === 0) {
+            notitfications_count += 1;
+        }
+    });
+    await refresh_notification();
+
     website_title.addEventListener("click", () => {
         window.location.href = "/";
     });

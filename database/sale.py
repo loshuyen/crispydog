a87a87312b2812e09ef185758bca9c015826dc07
Shuyen Lo <sycw723@gmail.com>
2024-08-29 01:20:33 +0800

    Modify sale API data structure.

--- database/sale.py
+++ database/sale.py
@@ -6,23 +6,30 @@ def get_all_sales(user_id):
         db = pool.get_connection()
         cursor = db.cursor()
         cursor.execute("""
-            SELECT product.id, product.name, product.price, COUNT(sale.id), product.thumbnail_url, product.status 
-            FROM product LEFT JOIN sale ON sale.product_id = product.id
+            SELECT sale.id, sale.buyer_id, sale.created_at, user.username, product.id, product.name, product.price, product.thumbnail_url
+            FROM product INNER JOIN sale ON sale.product_id = product.id
+            INNER JOIN user ON sale.buyer_id = user.id
             WHERE product.owner_id = %s
-            GROUP BY product.id
-            ORDER BY product.created_at DESC;""", (user_id, ))
+            ORDER BY sale.created_at DESC;
+        """, (user_id, ))
         sales = cursor.fetchall()
         result = []
         for sale in sales:
-            product_id, product_name, product_price, sale_count, product_thumbnail, product_status = sale
+            sale_id, sale_buyer_id, sale_created_at, user_username, product_id, product_name, product_price, product_thumbnail_url = sale
             result.append({
-                "product": {
-                    "id": product_id,
-                    "name": product_name,
-                    "price": product_price,
-                    "thumbnail": product_thumbnail,
-                    "status": product_status,
-                    "sales": sale_count
+                "sale": {
+                    "id": sale_id,
+                    "created_at": sale_created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                    "buyer": {
+                        "id": sale_buyer_id,
+                        "username": user_username
+                    },
+                    "product": {
+                        "id": product_id,
+                        "name": product_name,
+                        "price": product_price,
+                        "thumbnail": product_thumbnail_url,
+                    }
                 }
             })
         return result 
@@ -37,39 +44,55 @@ def get_sales(user_id, product_id):
         db = pool.get_connection()
         cursor = db.cursor()
         cursor.execute("""
-            SELECT product.id, product.name, product.price, product.thumbnail_url, product.status, user.username, user.id, sale.id, sale.created_at, review.id, review.rating, review.content, review.updated_at 
-            FROM product 
-            INNER JOIN sale ON sale.product_id = product.id
+            SELECT sale.id, sale.buyer_id, sale.created_at, user.username, review.id, review.rating, review.content, review.updated_at
+            FROM sale
             INNER JOIN user ON sale.buyer_id = user.id
-            INNER JOIN review ON review.product_id = product.id
-            WHERE product.owner_id = %s AND product.id = %s;""", (user_id, product_id))
+            INNER JOIN product ON sale.product_id = product.id
+            INNER JOIN review ON review.reviewer_id = sale.buyer_id
+            WHERE product.owner_id = %s AND sale.product_id = %s
+            ORDER BY sale.created_at DESC;
+        """, (user_id, product_id))
         sales = cursor.fetchall()
-        result = []
+        sales_result = []
         for sale in sales:
-            product_id, product_name, product_price, product_thumbnail_url, product_status, buyer_username, buyer_id, sale_id, sale_created_at, review_id, review_rating, review_content, review_updated_at = sale
-            result.append({
-                "product": {
-                    "id": product_id,
-                    "name": product_name,
-                    "price": product_price,
-                    "thumbnail": product_thumbnail_url,
-                    "status": product_status,
-                },
-                "buyer": {
-                    "id": buyer_id,
-                    "name": buyer_username,
-                },
+            sale_id, sale_buyer_id, sale_created_at, user_username, review_id, review_rating, review_content, review_updated_at = sale
+            sales_result.append({
                 "sale": {
                     "id": sale_id,
-                    "created_at": sale_created_at.strftime("%Y-%m-%d %H:%M:%S")
-                },
-                "review": {
-                    "id": review_id,
-                    "rating": review_rating,
-                    "content": review_content,
-                    "updated_at": review_updated_at.strftime("%Y-%m-%d %H:%M:%S")
+                    "created_at": sale_created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                    "buyer": {
+                        "id": sale_buyer_id,
+                        "username": user_username
+                    },
+                    "review": {
+                        "id": review_id,
+                        "rating": review_rating,
+                        "content": review_content,
+                        "updated_at": review_updated_at.strftime("%Y-%m-%d %H:%M:%S")
+                    }
                 }
             })
+        cursor.execute("""
+            SELECT product.id, COUNT(sale.id), product.name, product.price, product.thumbnail_url,product.status
+            FROM sale INNER JOIN product ON sale.product_id = product.id
+            WHERE product.owner_id = %s AND product.id = %s
+            ORDER BY sale.created_at DESC;
+        """, (user_id, product_id))
+        product = cursor.fetchall()[0]
+        if product[0] == None:
+            return None
+        id, sales_count, product_name, product_price, product_thumbnail_url, product_status = product
+        result = {
+            "product": {
+                "id": product_id,
+                "name": product_name,
+                "price": product_price,
+                "thumbnail": product_thumbnail_url,
+                "status": product_status,
+                "sales_count": sales_count,
+                "sales": sales_result
+            }
+        }
         return result 
     except Exception as e:
         raise e
